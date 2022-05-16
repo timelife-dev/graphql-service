dc3c95e15762a3a388820b0d5aaf7687e0e4e916
Hugo Rodriguez <44950015+hugorodro@users.noreply.github.com>
2022-05-16 17:04:00 -0400

    add new app code for sessions

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,40 @@
 import "reflect-metadata";
+import * as http from "http";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-express";
-import express from "express";
 import { buildSchema } from "type-graphql";
-import * as http from "http";
+import { createConnection } from "typeorm";
+import { MyContext } from "./types";
 import { TaskResolver } from "./resolvers/task";
 import { Task } from "./entities/Task";
-import { createConnection } from "typeorm";
+import { UserResolver } from "./resolvers/user";
+import { User } from "./entities/User";
+import express from "express";
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
-const CONTAINER_PORT = 8080;
 const main = async () => {
+  
+  const HOST = "localhost";
+  const CONTAINER_PORT = 8080;
+  const DB_NAME = "postgres";
+  const DB_USER = "postgres";
+  const DB_PASSWORD = "postgres";
+  const DB_PORT = 5432;
+  const REDIS_URL = "127.0.0.1:6379";
+  const REDIS_SECRET = "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81";
+
   const conn = createConnection({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    database: "postgres",
-    username: "postgres",
-    password: "postgres",
+    host: HOST,
+    port: DB_PORT,
+    database: DB_NAME,
+    username: DB_USER,
+    password: DB_PASSWORD,
     logging: true,
-    synchronize: true,
-    entities: [Task],
+    synchronize: false,
+    entities: [Task, User],
   });
 
   await (
@@ -27,25 +42,67 @@ const main = async () => {
   ).runMigrations;
 
   const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = new Redis(REDIS_URL);
+
+  app.set("trust proxy", 1);
+  // app.use(
+  //   cors({
+  //     origin:"http://localhost8080",
+  //     credentials: true
+  //   })
+  // )
+  app.set('trust proxy', true)
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30,
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      },
+      secret: REDIS_SECRET,
+      saveUninitialized: false,
+      resave: false,
+    })
+  );
+
   const httpServer = http.createServer(app);
+
   const apolloServer = new ApolloServer({
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     schema: await buildSchema({
-      resolvers: [TaskResolver],
+      resolvers: [TaskResolver, UserResolver],
       validate: false,
     }),
+    context: ({ req, res }): MyContext => ({ req, res }),
   });
+
 
   await apolloServer.start();
 
-  apolloServer.applyMiddleware({ app });
+  const corsOptions = {
+    origin: "https://studio.apollographql.com",
+    credentials: true,
+  };
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: corsOptions,
+  });
 
   await new Promise<void>((resolve) =>
     httpServer.listen({ port: CONTAINER_PORT }, resolve)
   );
 
   console.log(
-    `🚀 Server ready at http://localhost:${CONTAINER_PORT}${apolloServer.graphqlPath}`
+    `🚀 Server ready at http://${HOST}:${CONTAINER_PORT}${apolloServer.graphqlPath}`
   );
 
   return { apolloServer, app };
